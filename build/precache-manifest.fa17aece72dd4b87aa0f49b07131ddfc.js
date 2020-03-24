a7afa884e675f118ec550f7bf788a8bede966175
Алексей Мороз <alexeymoroz@Alex-Moroz.local>
2020-03-24 15:43:51 +0200

    first commit

---
+++ build/precache-manifest.fa17aece72dd4b87aa0f49b07131ddfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a1343bb670924c58b4ae6d099cd425",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "b52c472ce66a5cdb9061",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.f595e781.chunk.css"
+  },
+  {
+    "revision": "e03841d80fd1d895a9a4",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.78823d43.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.78823d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52c472ce66a5cdb9061",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.428323f2.chunk.js"
+  },
+  {
+    "revision": "fc9c0c41fe25565dd363",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.974f980a.js"
+  }
+]);
